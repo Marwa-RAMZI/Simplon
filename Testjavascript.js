39958f07ec9bb69c5808fefa01f50f086c037620
Marwa-RAMZI <marwaramzi37@gmail.com>
2024-11-22 09:31:46 +0100

    testjs

--- Testjavascript.js
+++ Testjavascript.js
@@ -1,3 +1,12 @@
-for(let i=1 ; i<15 ; i+=2){
+//Exercice1
+for(let i=1 ; i<30 ; i+=3){
     console.log(i);
 }
+
+//Exercice2
+
+const array= ["banana", "fraise", "poivre"];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i].toUpperCase());
+}
+
